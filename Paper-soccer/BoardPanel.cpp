4be21fc60718281733e75600fdd5768c382ea4d3
Kamil <100875852+kamilb13@users.noreply.github.com>
2023-05-12 22:51:22 +0200

    Update BoardPanel.cpp

--- Paper-soccer/BoardPanel.cpp
+++ Paper-soccer/BoardPanel.cpp
@@ -14,7 +14,7 @@ BoardPanel::BoardPanel(wxFrame* parent, const wxString& title) : wxPanel(parent,
 	wxColour* disabledBtnColor = new wxColour(50, 50, 50, 0);
 	
 	//CreateStatusBar();
-
+	//wxLogStatus(wxString::Format("%d", player));
 	wxClientDC line(this);
 	line.SetPen(boardLine);
 
@@ -23,16 +23,45 @@ BoardPanel::BoardPanel(wxFrame* parent, const wxString& title) : wxPanel(parent,
 
 	line.DrawLine(source, destiny);
 	
+	// WYŚWIETLANIE GÓREJ BRAMKI
+	wxButton* btnTopGoal1 = new wxButton(this, wxID_ANY, " ", wxPoint(80, 20), wxSize(10, 10));
+	wxButton* btnTopGoal2 = new wxButton(this, wxID_ANY, " ", wxPoint(110, 20), wxSize(10, 10));
+	auto boundCosTam = std::bind(&BoardPanel::drawLine, this, std::placeholders::_1, &(board->current_x), &(board->current_y), -1, -1, board, &player);
+	btnTopGoal2->Bind(wxEVT_BUTTON, boundCosTam);
+
+	wxButton* btnTopGoal3 = new wxButton(this, wxID_ANY, " ", wxPoint(140, 20), wxSize(10, 10));
+	//wxButton* btnBottomGoal1 = new wxButton(this, wxID_ANY, " ", wxPoint(20 + 30 * 2, 20 + 30 * 1), wxSize(10, 10));
+	board->getTopGoal1()->setFieldBtn(btnTopGoal1);
+	board->getTopGoal2()->setFieldBtn(btnTopGoal2);
+	board->getTopGoal3()->setFieldBtn(btnTopGoal3);
+	
+	// WYŚWIETLANIE DOLNEJ BRAMKI
+	wxButton* btnBottomGoal1 = new wxButton(this, wxID_ANY, " ", wxPoint(80, 50 + 30 * board->getY()), wxSize(10, 10));
+	wxButton* btnBottomGoal2 = new wxButton(this, wxID_ANY, " ", wxPoint(110, 50 + 30 * board->getY()), wxSize(10, 10));
+	auto boundCosTamBottom = std::bind(&BoardPanel::drawLine, this, std::placeholders::_1, &(board->current_x), &(board->current_y), -1, -1, board, &player);
+	btnBottomGoal2->Bind(wxEVT_BUTTON, boundCosTamBottom);
+
+	wxButton* btnBottomGoal3 = new wxButton(this, wxID_ANY, " ", wxPoint(140, 50 + 30 * board->getY()), wxSize(10, 10));
+	//wxButton* btnBottomGoal1 = new wxButton(this, wxID_ANY, " ", wxPoint(20 + 30 * 2, 20 + 30 * 1), wxSize(10, 10));
+	board->getBottomGoal1()->setFieldBtn(btnBottomGoal1);
+	board->getBottomGoal2()->setFieldBtn(btnBottomGoal2);
+	board->getBottomGoal3()->setFieldBtn(btnBottomGoal3);
+	
 	for (int i = 0; i < board->getY(); i++) {
 		for (int j = 0; j < board->getX(); j++) {
-			wxButton* fieldButton = new wxButton(this, wxID_ANY, " ", wxPoint(20 + 30 * j, 20 + 30 * i), wxSize(10, 10));
+			if (j == 0 || i == 0 || j == board->getX() - 1 || i == board->getY() - 1 ) {	// Nadawanie skrajnym pozycjom board setVisited na true
+				board->getFieldAt(j, i)->setVisited(true);
+			}
+
+			wxButton* fieldButton = new wxButton(this, wxID_ANY, " ", wxPoint(20 + 30 * j, 50 + 30 * i), wxSize(10, 10));
+			
+
 			board->getFieldAt(j, i)->setFieldBtn(fieldButton);
 			board->getFieldAt(j, i)->getFieldBtn()->Disable();	//
 			board->getFieldAt(j, i)->getFieldBtn()->SetBackgroundColour(wxColour(210, 210, 210,1));	//
-			auto boundDrawLine = std::bind(&BoardPanel::drawLine, this, std::placeholders::_1, &(board->current_x), &(board->current_y), j, i, board);
+			auto boundDrawLine = std::bind(&BoardPanel::drawLine, this, std::placeholders::_1, &(board->current_x), &(board->current_y), j, i, board, &player);
 			fieldButton->Bind(wxEVT_BUTTON, boundDrawLine);
 		}
-
 	}
 
 	// JEZELI ISTNIEJA (PRZEROBIC)
@@ -61,19 +90,29 @@ BoardPanel::BoardPanel(wxFrame* parent, const wxString& title) : wxPanel(parent,
 	board->getCurrentField().getPD()->getFieldBtn()->SetBackgroundColour(wxColour(255, 0, 0, 1));
 }
 
-
-
-void BoardPanel::drawLine(wxCommandEvent& event, int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board) 
+void BoardPanel::drawLine(wxCommandEvent& event, int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board, bool* player) 
 {
-		wxClientDC dc(this);
-		// ustawienie koloru linii na czerwony o grubo�ci 1 piksela
+	if (destiny_x == -1) {
+		wxLogStatus("KONIEC");
+		wxWindow::Destroy();
+
+		return;
+	}
+
+
+	wxClientDC dc(this);
+	dc.SetPen(wxPen(wxColor(0, 0, 255), 3, wxPENSTYLE_SOLID));	// czerwony dla player == 0
+	if (*player == 1) {											// czerwony dla player == 1
 		dc.SetPen(wxPen(wxColor(255, 0, 0), 3, wxPENSTYLE_SOLID));
-		wxPoint source((*current_x * 30) + 20 + 5, (*current_y * 30) + 20 + 5);
-		wxPoint destiny(destiny_x * 30 + 20 + 5, destiny_y * 30 + 20 + 5);
-		dc.DrawLine(source, destiny);
+	}
+
+	// ustawienie koloru linii na czerwony o grubości 1 piksela	
+	wxPoint source((*current_x * 30) + 20 + 5, (*current_y * 30) + 50 + 5);
+	wxPoint destiny(destiny_x * 30 + 20 + 5, destiny_y * 30 + 50 + 5);
+	dc.DrawLine(source, destiny);
 		
-		this->blockField(current_x, current_y, destiny_x, destiny_y, board);
-		this->switchCurrentField(current_x, current_y, destiny_x, destiny_y, board);
+	this->blockField(current_x, current_y, destiny_x, destiny_y, board);
+	this->switchCurrentField(current_x, current_y, destiny_x, destiny_y, board, player);
 }
 
 void BoardPanel::blockField(int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board) {
@@ -128,8 +167,22 @@ void BoardPanel::isWin(int* current_x, int* current_y, int destiny_x, int destin
 }
 */
 
-void BoardPanel::switchCurrentField(int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board)
+void BoardPanel::switchPlayer(Field* field, bool* player, Board* board) {
+	if (!field->getVisited()) {
+		bool temp = !(*player);
+		*player = temp;
+	}
+
+	wxLogStatus(wxString::Format("%d", *player));
+
+}
+
+
+void BoardPanel::switchCurrentField(int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board, bool* player)
 {
+
+	
+
 	wxColour enabledButton(255, 0, 0, 1);
 	wxColour disabledButton(210, 210, 210, 1);
 	*current_x = destiny_x;
@@ -177,6 +230,15 @@ void BoardPanel::switchCurrentField(int* current_x, int* current_y, int destiny_
 	
 	board->setCurrentField(*(board->getFieldAt(destiny_x, destiny_y)));
 
+	switchPlayer(board->getFieldAt(destiny_x, destiny_y), player, board);
+
+	
+	board->getFieldAt(destiny_x, destiny_y)->setVisited(true);
+	wxLogStatus(wxString::Format("%d", *player));
+	
+
+	
+
 	// JEZELI ISTNIEJA (PRZEROBIC)
 	if (board->getCurrentField().getLG()) {
 		board->getCurrentField().getLG()->getFieldBtn()->Enable();
@@ -190,7 +252,6 @@ void BoardPanel::switchCurrentField(int* current_x, int* current_y, int destiny_
 	}
 	
 	if (board->getCurrentField().getPG()) {
-
 		board->getCurrentField().getPG()->getFieldBtn()->Enable();
 		board->getCurrentField().getPG()->getFieldBtn()->SetBackgroundColour(enabledButton);
 	}
@@ -206,7 +267,6 @@ void BoardPanel::switchCurrentField(int* current_x, int* current_y, int destiny_
 	}
 		
 	if (board->getCurrentField().getLD()) {
-
 		board->getCurrentField().getLD()->getFieldBtn()->Enable();
 		board->getCurrentField().getLD()->getFieldBtn()->SetBackgroundColour(enabledButton);
 	}
@@ -222,6 +282,5 @@ void BoardPanel::switchCurrentField(int* current_x, int* current_y, int destiny_
 	}
 		
 }
-
 
 
