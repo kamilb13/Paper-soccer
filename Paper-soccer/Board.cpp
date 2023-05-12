b995cf5a065d686ed1f85d7335dd968760d781d6
Kamil <100875852+kamilb13@users.noreply.github.com>
2023-05-12 22:50:42 +0200

    Update Board.cpp

--- Paper-soccer/Board.cpp
+++ Paper-soccer/Board.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 
+
 using namespace std;
 
 Board::Board(int x, int y) {
@@ -24,15 +25,14 @@ Board::Board(int x, int y) {
 
 	//this->currentField = this->fields[y / 2][x / 2]);
 
-
-	
-
 	for (int i = 0; i < y; i++)
 	{
 		for (int j = 0; j < x; j++) {
 
-			// Tworzenie przycisk�w
+			// Tworzenie przycisków
+			this->fields[i][j].setVisited(false);
 
+			
 
 			if (i == 0) {          // USTAWIAM WSKAZNIKI W PIERWSZYM WIERSZU
 				if (j == 0) {
@@ -101,10 +101,51 @@ Board::Board(int x, int y) {
 			}
 		}
 	}
-	
 
-	
+	// PRZYPISYWANIE WSKAŹNIKÓW DLA BRAMEK 
 
+	//BRAMKA TOP
+	
+	this->fields[0][x / 2].setG(&topGoal2);
+	this->fields[0][x / 2].setLG(&topGoal1);
+	this->fields[0][x / 2].setPG(&topGoal3);
+
+	this->fields[0][(x / 2) - 1].setPG(&topGoal2);
+	this->fields[0][(x / 2) + 1].setLG(&topGoal2);
+
+	//BRAMKA BOTTOM
+	this->fields[y - 1][x / 2].setD(&bottomGoal2);
+	this->fields[y - 1][x / 2].setLD(&bottomGoal1);
+	this->fields[y - 1][x / 2].setPD(&bottomGoal3);
+
+	this->fields[y - 1][(x / 2) - 1].setPD(&bottomGoal2);
+	this->fields[y - 1][(x / 2) + 1].setLD(&bottomGoal2);
+	
+}
+
+
+Field* Board::getTopGoal1() {
+	return &(this->topGoal1);
+}
+
+Field* Board::getTopGoal2() {
+	return &(this->topGoal2);
+}
+
+Field* Board::getTopGoal3() {
+	return &(this->topGoal3);
+}
+
+Field* Board::getBottomGoal1() {
+	return &(this->bottomGoal1);
+}
+
+Field* Board::getBottomGoal2() {
+	return &(this->bottomGoal2);
+}
+
+Field* Board::getBottomGoal3(){
+	return &(this->bottomGoal3);
 }
 
 void Board::setCurrentField(Field field)
