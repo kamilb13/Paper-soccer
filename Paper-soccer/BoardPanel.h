1081d41e29ab079e66b8bd612813ddfa4c8639c8
Kamil <100875852+kamilb13@users.noreply.github.com>
2023-05-12 22:51:36 +0200

    Update BoardPanel.h

--- Paper-soccer/BoardPanel.h
+++ Paper-soccer/BoardPanel.h
@@ -8,10 +8,11 @@ using namespace std;
 class BoardPanel : public wxPanel
 {
 private:
-	
+	bool player = true;
 public:
 	BoardPanel(wxFrame* parent, const wxString& title); 
-	void drawLine(wxCommandEvent& event, int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board);
-	void switchCurrentField(int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board);
+	void drawLine(wxCommandEvent& event, int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board, bool* player);
+	void switchPlayer(Field* field, bool* player, Board* board);
+	void switchCurrentField(int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board, bool* player);
 	void blockField(int* current_x, int* current_y, int destiny_x, int destiny_y, Board* board);
 };
