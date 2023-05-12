5863c7147945ff9af10d0b565839f693579d9731
Kamil <100875852+kamilb13@users.noreply.github.com>
2023-05-12 22:51:02 +0200

    Update Board.h

--- Paper-soccer/Board.h
+++ Paper-soccer/Board.h
@@ -11,8 +11,21 @@ private:
 	int x, y;
 	vector<vector<Field>> fields;
 	Field currentField;
+	Field topGoal1;
+	Field topGoal2;
+	Field topGoal3;
+	Field bottomGoal1;
+	Field bottomGoal2;
+	Field bottomGoal3;
+	
 
 public:
+	Field* getTopGoal1();
+	Field* getTopGoal2();
+	Field* getTopGoal3();
+	Field* getBottomGoal1();
+	Field* getBottomGoal2();
+	Field* getBottomGoal3();
 	int current_x, current_y;
 	Board(int x, int y);
 	void setCurrentField(Field field);
