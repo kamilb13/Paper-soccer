f7e5561e908987e9a35b39e76428bfa0f1ec1651
Kamil <100875852+kamilb13@users.noreply.github.com>
2023-05-12 22:52:25 +0200

    Update MainFrame.cpp

--- Paper-soccer/MainFrame.cpp
+++ Paper-soccer/MainFrame.cpp
@@ -3,13 +3,28 @@
 #include "BoardFrame.h"
 #include "wx/wx.h"
 #include "BoardPanel.h"
+#include "StartPanel.h"
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
 {
-	// DO MAINFRAME Przenieœæ
+
+	//wxMediaCtrl* m_mediaCtrl = new wxMediaCtrl(this, wxID_ANY, "C:/Users/Komputer/Downloads/INTRO.avi",
+		//wxDefaultPosition, wxDefaultSize,0L,wxEmptyString, wxMEDIABACKEND_DIRECTSHOW,L"mediaCtrl",);
+
+	// Wczytanie pliku wideo
+	//m_mediaCtrl->Load("C:/Users/Komputer/Downloads/UEFA.mp4");
+
+	// Ustawienie rozmiaru i pozycji wxMediaCtrl
+	//m_mediaCtrl->SetSize(wxSize(400, 500));
+	//m_mediaCtrl->SetPosition(wxPoint(0, 0));
+	// DO MAINFRAME Przenieï¿½ï¿½
+	//StartPanel* startPanel = new StartPanel(this, "StartPanel");
+	CreateStatusBar();
 	BoardPanel* boardPanel = new BoardPanel(this, "BoardPanel");
 	
-	
+
+
+
 	//BoardFrame* boardFrame = new BoardFrame("Paper Soccer");
 	//StartFrame* startFrame = new StartFrame("Paper Soccer");
 	//boardFrame->SetClientSize(400, 500);
@@ -17,4 +32,3 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
 	//startFrame->Show();
 	//boardFrame->Show();
 }
-
